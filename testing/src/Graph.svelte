b775c5096f4e1996ae9141cc2e5b24ad88f90cda
DevinBayly <baylyd@email.arizona.edu>
2020-03-31 22:47:58 +0000

    finally got stop built in

--- testing/src/Graph.svelte
+++ testing/src/Graph.svelte
@@ -1,9 +1,11 @@
 <script>
-  let distanceTool = 1000;
+  let distanceTool = window.innerHeight / 3;
+  let off = false;
   let strengthTool = -509;
+  let threshold = 0;
   import * as d3 from "d3";
-  let width = 30000;
-  let height = 30000;
+  let width = window.innerWidth;
+  let height = window.innerHeight;
   let max = 0;
   let createGraph = (data, svgArg) => {
     let nodes = [];
@@ -33,8 +35,12 @@
       }
     }
 
-    const simulation = d3
-      .forceSimulation(nodes)
+    const simulation = d3.forceSimulation(nodes);
+    console.log(simulation)
+
+    simulation.stop();
+    console.log(simulation)
+    let forces = simulation
       .force(
         "link",
         d3
@@ -43,11 +49,11 @@
           .distance(1000)
       )
       .force("charge", d3.forceManyBody().strength(-509))
-      .force("center", d3.forceCenter(width / 2, height / 2))
-      .alphaTarget(1);
-
+      .force("center", d3.forceCenter(width / 2, height / 2));
     const svg = svgArg.attr("viewBox", [0, 0, width, height]);
 
+    console.log(simulation);
+    
     const zoom_group = svg.append("g");
 
     // make a color
@@ -59,8 +65,8 @@
       .selectAll("line")
       .data(links)
       .join("line")
-      .attr("stroke-width", d => d.value * 5)
-      .attr("color", d => d3.interpolateReds(d.value / max));
+      .attr("stroke-width", d => d.value)
+      .attr("stroke", d => d3.interpolateViridis(d.value / max));
 
     const node = zoom_group
       .append("g")
@@ -69,26 +75,43 @@
       .selectAll("circle")
       .data(nodes)
       .join("circle")
-      .attr("r", d => d.value * 5)
+      .attr("r", d => d.value)
       .attr("fill", "black");
 
     node.append("title").text(d => d.id);
 
-    simulation.on("tick", () => {
+    let ticked = () => {
       link
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
+        .attr("x1", d => Math.min(width - 100, Math.abs(d.source.x)))
+        .attr("y1", d => Math.min(height - 100, Math.abs(d.source.y)))
+        .attr("x2", d => Math.min(width - 100, Math.abs(d.target.x)))
+        .attr("y2", d => Math.min(height - 100, Math.abs(d.target.y)));
 
-      node.attr("cx", d => d.x).attr("cy", d => d.y);
-    });
+      node
+        .attr("cx", d => Math.min(width - 100, Math.abs(d.x)))
+        .attr("cy", d => Math.min(height - 100, Math.abs(d.y)));
+    };
+    setInterval(() => {
+      if (!off ) {
+        simulation.tick();
+        ticked();
+        simulation.alpha(1)
+        simulation.restart()
+      }
+    }, 1000);
     d3.interval(() => {
       simulation
         .nodes(nodes)
         .force("link", d3.forceLink(links).distance(distanceTool))
         .force("center", d3.forceCenter(width / 2, height / 2))
         .force("charge", d3.forceManyBody().strength(strengthTool));
+      link.attr("stroke-opacity", d => {
+        if (d.value > threshold) {
+          return 0.6;
+        } else {
+          return 0;
+        }
+      });
     }, 2000);
     svg.call(
       d3
@@ -111,21 +134,32 @@
     let node = createGraph(data, svg);
   };
 </script>
+
 <style>
-#holder {
-    position:fixed;
-    left:0;
-}</style>
+  #holder {
+      display:flex;
+  }
+</style>
+
+
 <div id="holder">
 
-<label>
-  <input type="number" bind:value={distanceTool} min="0" max="30000" />
-  <input type="range" bind:value={distanceTool} min="0" max="30000" />
-</label>
-<label>
-  <input type="number" bind:value={strengthTool} min="-10000" max="-30" />
-  <input type="range" bind:value={strengthTool} min="-10000" max="-30" />
-</label>
-
-</div>
 <div id="container" />
+<div id="labels">
+
+  <label>
+    <input type="number" bind:value={distanceTool} min="0" max="3000" />
+    <input type="range" bind:value={distanceTool} min="0" max="3000" />
+  </label>
+  <label>
+    <input type="number" bind:value={threshold} min="0" {max} />
+    <input type="range" bind:value={threshold} min="0" {max} />
+  </label>
+
+  <label>
+    <input type="number" bind:value={strengthTool} min="-10000" max="-30" />
+    <input type="range" bind:value={strengthTool} min="-10000" max="-30" />
+  </label>
+  <input type="checkbox" bind:checked={off} />
+</div>
+</div>
