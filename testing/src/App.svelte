e98fec618f87e3c7a292eb2f4637fdaae1314a9e
DevinBayly <baylyd@email.arizona.edu>
2020-04-03 10:47:54 -0700

    Include description and links

--- testing/src/App.svelte
+++ testing/src/App.svelte
@@ -1,6 +1,21 @@
 <script>
-	import Graph from "./Graph.svelte"
-	export let name;
+  import Graph from "./Graph.svelte";
+  export let name;
 </script>
 
-<Graph></Graph>
+<style>
+  img {
+    width: 20%;
+  }
+</style>
+
+<a href="http://rtdv-intake.glitch.me/">
+  <img src="rtdvicon.png" alt="" />
+</a>
+<h1>Biochem Networks</h1>
+<p>
+  This project was produced to assist a UArizona Biochemistry PhD candidate
+  <a href="https://cbc.arizona.edu/directory/people/brittany-stankavich">Brittany Stankavich</a> with her understanding of the relationships between
+  certain protein categories and their components
+</p>
+<Graph />
