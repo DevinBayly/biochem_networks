2a9e844bc33f503a101dbed7966ce3dcd9a5cd25
DevinBayly <baylyd@email.arizona.edu>
2020-04-15 12:13:03 -0700

    bunch of changes, but 2 legends

--- testing/src/Graph.svelte
+++ testing/src/Graph.svelte
@@ -4,9 +4,9 @@
   let strengthTool = -50;
   let threshold = 5;
   import * as d3 from "d3";
-  import { colorCanvas } from "./legend.js";
-  let width = 7000*2;
-  let height = 7000*2;
+  import legend from "d3-svg-legend"
+  let width = 7000 ;
+  let height = 7000;
   let max = 0;
   let createGraph = (data, svgArg) => {
     //remove previous
@@ -23,6 +23,9 @@
           target: othernode,
           value: data[nodename].edges[othernode].length
         };
+        if (el.value > max) {
+          max = el.value
+        }
         if (el.value >= threshold) {
           links.push(el);
           let node = {
@@ -33,9 +36,6 @@
             nodes.push(node);
             existingNames.push(node.id);
           }
-        }
-        if (max < el.value) {
-          max = el.value;
         }
       }
     }
@@ -72,20 +72,25 @@
       .selectAll("line")
       .data(links)
       .join("line")
-      .attr("stroke-width", d => d.value*10)
-      .attr("stroke", d => d3.interpolateViridis(d.value / max));
+      .attr("stroke-width", d => d.value*4)
+      .attr("stroke", d => d3.interpolateViridis((d.value - threshold)/(max-threshold)));
+    // make a binning for the values of the nodes
+    let minMaxVal;
+    let radiiRange = [50,250]
+    {
+      let vals = nodes.map(e => e.value);
+      minMaxVal = [Math.min(...vals), Math.max(...vals)];
+    }
+
+    const sizeScale = d3.scaleLinear(minMaxVal,radiiRange)
     const node = zoom_group
       .append("g")
       .selectAll("circle")
       .data(nodes)
       .join("circle")
-      .attr("stroke", d => {
-        //first get us down to a 0-10, floor to categorize, then div by 10 to interpolation range 0-1
-        return d3.interpolateViridis(Math.floor(d.value / 40) / 10);
-      })
-      .attr("stroke-width", 30.5)
-      .attr("r", d=> d.value)
-      .attr("fill-opacity", 0)
+      .attr("stroke-width", 3.5)
+      .attr("r", d => sizeScale(d.value))
+      .attr("fill-opacity",.5)
       .call(
         d3
           .drag()
@@ -93,6 +98,45 @@
           .on("drag", dragged)
           .on("end", dragended)
       );
+    // create size legend for nodes
+
+    let offsets = radiiRange[1]+50
+    svg
+      .append("g")
+      .attr("class", "legendSize")
+      .attr("transform", "translate("+150+","+offsets+")");
+    var legendSize = legend.legendSize().scale(sizeScale)
+      .shape("circle")
+      .shapePadding(offsets)
+      .labelOffset(offsets)
+      .orient("horizontal")
+      .title("Category size using sum total of unique proteins");
+
+
+    svg.select(".legendSize").call(legendSize);
+    // now the edge legend, put at the bottom
+          
+var viridisScale = d3.scaleSequential(d3.interpolateViridis)
+  .domain([threshold,max])
+
+
+svg.append("g")
+  .attr("class", "legendViridis")
+  .attr("transform", `translate(${30},${1300})`);
+
+var legendViridis = legend.legendColor()
+    .shapeWidth(width/(4*(max-threshold +1)))
+    .shapeHeight(200)
+    .cells(max-threshold + 1)
+    .labelOffset(180)
+    .orient("horizontal")
+    .scale(viridisScale) 
+    .title("Number of shared unique Proteins")
+
+svg.select(".legendViridis")
+  .call(legendViridis);
+          
+        
     function dragstarted(d) {
       if (!d3.event.active) simulation.alphaTarget(0.3).restart(); //sets the current target alpha to the specified number in the range [0,1].
       d.fy = d.y; //fx - the node’s fixed x-position. Original is null.
@@ -110,7 +154,7 @@
       if (!d3.event.active) simulation.alphaTarget(0);
       d.fx = d3.event.x;
       d.fy = d3.event.y;
-      d.moused= true
+      d.moused = true;
       console.log("data after dragged is ...", data);
     }
 
@@ -121,8 +165,8 @@
       .enter()
       .append("text")
       .text(d => d.id.replace(/hsa.*? /, ""))
-      .attr("x", d => d.x + (Math.floor(d.value / 40) + 1) * 5)
-      .attr("y", d => d.y + (Math.floor(d.value / 40) + 1) * 5)
+      .attr("x", d => d.x + sizeScale(d.value) +200)
+      .attr("y", d => d.y + sizeScale(d.value) +200)
       .attr("dy", 5);
 
     let ticked = () => {
@@ -141,24 +185,23 @@
         .attr("y", d => d.y + (Math.floor(d.value / 40) + 1) * 5)
         .attr("dy", 5);
     };
-    let prevIter
+    let prevIter;
     setInterval(() => {
       if (off) {
-        nodes.map(node=> {
-          node.fx = node.x
-          node.fy = node.y
-        })
-        prevIter = off
+        nodes.map(node => {
+          node.fx = node.x;
+          node.fy = node.y;
+        });
+        prevIter = off;
       }
       if (!off && prevIter) {
         // go through and change all the not mouse spec ones back to non fx
-        nodes.map(node=> {
+        nodes.map(node => {
           if (!node.moused) {
-            node.fx = null
-            node.fy = null
+            node.fx = null;
+            node.fy = null;
           }
-        })
-
+        });
       }
       simulation.tick();
       ticked();
@@ -194,9 +237,6 @@
     svg = d3.select("#container").append("svg");
     createGraph(data, svg);
 
-    setTimeout(() => {
-      colorCanvas();
-    }, 2000);
   };
   window.onload = async () => {
     data = await fetch("./total_counts_version.json").then(res => res.json());
@@ -264,8 +304,4 @@
     </label>
     <button id="download">Download svg</button>
   </div>
-</div>
-<p>Number of shared elements in category</p>
-<div id="legend">
-  <canvas />
 </div>
